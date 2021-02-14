e0d352fe4775c512b0d38d3779c0db33e5532883
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-02-14 14:06:41 -0800

    remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,12 @@
 jQuery(function ($) {
 
-  $('.clientCarusel').owlCarousel({
-    autoplay: true,
-    loop: false,
-    margin: 15,
-    rtl:true,
-    dots:false,
-    navText : ["<i class='lni lni-arrow-left'></i>","<i class='lni lni-arrow-right'></i>"],
 
-    responsive: {
-      0: {
-        items: 1,
-
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-
-      }
-      
-
-    }
-  });
- 
   function onScroll(event){
-			
+			//add class active when click on navbar item
 		var scrollPosition = $(document).scrollTop();
 		$('.navbar-collapse .nav-item a[href^="#"').each(function () {
 			console.log($(this).attr('href')); 
 		  var refElement = $($(this).attr("href"));
-		//   console.log($(this).attr("href")); //log
 		  if (refElement.length&&refElement.position().top <= scrollPosition) {
 			$('.navbar-collapse .nav-item ').removeClass("active");
 			$(this).parent().addClass("active");
@@ -43,20 +18,18 @@ jQuery(function ($) {
 		});
 	  }
     $(document).on("scroll", onScroll);
-
+//animate to section when click
     $('.navbar-collapse ul li a').on('click', function () {
       $('html, body').animate({ scrollTop: $(this.hash).offset().top  - 5 }, 1000);
       $('.navbar-collapse.collapse.in').removeClass('in');
       return false;
     });
-    
+        // add fix navbar when scroll down and remove it on top 
   $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
 
     if (scrollTop > $('header').height()) {
-      $("#counter").css("padding-top", "150px");
       $('header').addClass('scrollNav');
-
 
     }
 
@@ -65,6 +38,9 @@ jQuery(function ($) {
     }
 
   });
+
+    
+  //close mobile navbar when chooe one of items
   $(".navbar .nav-link").click(function(){
     $(".collapse").removeClass("show");
   })
